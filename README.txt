08a23c0416691edd556a95b3d45b57af260691ff
Ezwan Harun <eharun@adaptavist.com>
2022-04-01 16:17:23 +0800

    modify the framework

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-This automation framework script has been written by Ezwan and it specifically for Adaptavist Technical Test task 2.
+This automation framework script was acted as template for Adaptavist automation engineer candidates to start writing automation for interview home task.
 
 Please download chromedriver in here https://chromedriver.chromium.org/downloads if it not compatible with your chrome browser.
 
@@ -11,14 +11,9 @@ The components that has been used in building up this automation framework was :
 -Chrome
 -Maven
 
-This solution contains several automation test cases against severals Herokuapp scenarios as below:
+This project contains sample solutions against the Herokuapp website scenarios as below:
 - Login with valid credentials
 - Login with invalid credentials
-- Test the JS alert button
-- Test the JS prompt button
-- Test the JS confirm button
-- Test writing text inside iframe
-- Test selecting editor toolbar
 
 Step to run:
 Please use 'mvn clean install' command from the terminal in order to download dependencies, build, and run the test
--- src/test/java/Steps/herokuStepDefs.java
+++ src/test/java/Steps/herokuStepDefs.java
@@ -1,7 +1,5 @@
 package Steps;
 
-import Pages.herokuappIFramePage;
-import Pages.herokuappJSAlertPage;
 import Pages.herokuappLoginFormPage;
 import Pages.herokuappSecurePage;
 import io.cucumber.java.After;
@@ -18,12 +16,10 @@ public class herokuStepDefs {
     private WebDriver driver;
     private herokuappLoginFormPage loginpage;
     private herokuappSecurePage securePage;
-    private herokuappJSAlertPage alertPage;
-    private herokuappIFramePage iframePage;
 
     @Before
     public void startUp(){
-        System.setProperty("webdriver.chrome.driver","src/test/java/Resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","src/test/java/Resources/chromedriver");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
     }
@@ -36,21 +32,10 @@ public class herokuStepDefs {
         }
     }
 
-    @Given ("^I navigate to herokuapp '(.*)' website$")
-    public void navigateToJSAlertWebsite(String site){
-        if(site.equals("alert")){
-            alertPage = new herokuappJSAlertPage(driver);
-            alertPage.navigateToWebsite();
-        }
-        else if(site.equals("login")){
-            loginpage = new herokuappLoginFormPage(driver);
-            loginpage.navigateToWebsite();
-        }
-        else if(site.equals("iframe")){
-            iframePage = new herokuappIFramePage(driver);
-            iframePage.navigateToWebsite();
-        }
-
+    @Given ("^I navigate to herokuapp website$")
+    public void navigateToJSAlertWebsite(){
+        loginpage = new herokuappLoginFormPage(driver);
+        loginpage.navigateToWebsite();
     }
 
     @When("^I insert '(.*)' user credentials$")
@@ -63,39 +48,9 @@ public class herokuStepDefs {
         }
     }
 
-    @When("^I click JS '(.*)' button$")
-    public void clickJSButton(String jsAlertButton){
-        alertPage.clickAlertButton(jsAlertButton);
-    }
-
-    @When("^I write '(.*)' in the text iframe$")
-    public void writeText(String text){
-        iframePage.writeTextInIFrame(text);
-    }
-
     @And("I click login button")
     public void clickLogin(){
         loginpage.clickLoginButton();
-    }
-
-    @And("^I click '(.*)' button on '(.*)' warning popout$")
-    public void handleBrowserAlert(String buttonType, String alertType){
-        alertPage.clickAlertMessage(alertType,buttonType);
-    }
-
-    @And("^I insert '(.*)' as input value$")
-    public void insertInputOnAlert(String input){
-        alertPage.insertInputField(input);
-    }
-
-    @And("^I click '(.*)' on toolbar$")
-    public void clickToolbar(String toolbarType){
-        iframePage.clickToolbar(toolbarType);
-    }
-
-    @Then("^Result display '(.*)'$")
-    public void checkResult(String message){
-        alertPage.checkResultDisplay(message);
     }
 
     @Then ("^I '(.*)' logon into herokuapp account$")
@@ -107,15 +62,5 @@ public class herokuStepDefs {
         else{
             securePage.verifyUserAuthorization();
         }
-    }
-
-    @Then("^result will show '(.*)' was selected$")
-    public void verifySelectedToolbar(String toolbarType){
-        iframePage.checkSelectedToolbar(toolbarType);
-    }
-
-    @And("^verify '(.*)' are written in iframe$")
-    public void verifyTextInput(String text){
-        iframePage.verifyText(text);
     }
 }
